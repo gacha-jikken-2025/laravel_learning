0a8902be4c0cbab2f0c6f6d87558a76b0cc437fb
kbys-jikken2025 <nokoyama240@gmail.com>
2025-09-14 13:05:32 +0900

    style調整

--- laravelapp/resources/views/gacha/gacha.blade.php
+++ laravelapp/resources/views/gacha/gacha.blade.php
@@ -1,22 +1,38 @@
-<div style="text-align: center;">
-    レア排出確率{{$rate * 100}}%
-    <h1>{{$result->item_name}}</h1>
-    <p>{{$result->rarity}}</p>
-    <img src="{{ asset($result->image) }}" alt="">
-</div>
-<div style="display: flex; gap: 1rem; justify-content: center; text-align: center;">
-    <p>
-        <a href="/gacha/1">
-            ガチャ1
-        </a>
-        <br />
-        レア排出確率 30%
-    </p>
-    <p>
-        <a href="/gacha/2">
-            ガチャ2
-        </a>
-        <br />
-        レア排出確率 60%
-    </p>
-</div>
+<html>
+    <head>
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    </head>
+    <body>
+        <div class="container">
+            <div class="head">
+                <h1>武器ガチャ</h1>
+                <p>レア排出確率{{ $rate * 100 }}%</p>
+            </div>
+
+            <div class="result" data-rarity="{{ $result->rarity }}">
+                <div class="result_head">
+                    <h2>{{ $result->item_name }}</h2>
+                    <p class="result_rarity">{{ $result->rarity }}</p>
+                </div>
+                <figure class="result_image">
+                    <img src="{{ asset($result->image) }}" alt="{{ $result->item_name }}">
+                </figure>
+            </div>
+            
+            <div class="foot">
+                <p class="foot_item">
+                    <a href="/gacha/1">
+                        ガチャ1
+                    </a>
+                    レア排出確率 30%
+                </p>
+                <p class="foot_item">
+                    <a href="/gacha/2">
+                        ガチャ2
+                    </a>
+                    レア排出確率 60%
+                </p>
+            </div>
+        </div>
+    </body>
+</html>
