4ba5c0d9ada4f9ea2e3d128b293285f7c1457f61
gacha-jikken-2025 <nokoyama240@gmail.com>
2026-01-01 22:43:38 +0900

    gachaコントローラーにtry catchの処理設定

--- laravelapp/app/Http/Controllers/GachaController.php
+++ laravelapp/app/Http/Controllers/GachaController.php
@@ -5,39 +5,50 @@ namespace App\Http\Controllers;
 // Productモデルを呼び出す
 use App\Models\Gacha;
 use App\Models\Item;
+use Illuminate\Http\Request;
 
 class GachaController extends Controller
 {
 
-    public function show($str){
+    public function show(Request $request, $id){
 
-        // ガチャの種類
-        $box = Gacha::box_select($str);
+        $request->merge(['id' => $id]);
 
-        // ガチャのレア確率
-        $box_rate = $box->rate;
+        $request->validate([
+            'id' => ['required', 'integer', 'min:1', 'max:2'],
+        ]);
 
-        // ガチャで出た値
-        $draw = rand(1, 100) * 0.01;
+        try {
+            // ガチャの種類
+            $box = Gacha::box_select($id);
 
-        // レアかノーマルかの抽選
-        if($box_rate > $draw) {
-            $rarity = '1';
+            // ガチャのレア確率
+            $box_rate = $box->rate;
+
+            // ガチャで出た値
+            $draw = rand(1, 100) * 0.01;
+
+            // レアかノーマルかの抽選
+            if($box_rate > $draw) {
+                $rarity = '1';
+            }
+            else {
+                $rarity = '0';
+            }
+
+            // 抽選されたアイテム
+            // $result = Item::item_draw($rarity);
+
+            // 抽選されたレアリティで絞り込み
+            $item_box = Item::where('rarity', '=', $rarity);
+
+            // 絞り込みされたアイテムの中からランダムで取得
+            $result = $item_box->inRandomOrder()->get()->first();
+            
+            // index.blade.phpに$productsをわたす
+            return view('gacha/gacha', ['result' => $result, 'rate' => $box_rate]);
+        } catch (\Exception $e) {
+            return false;
         }
-        else {
-            $rarity = '0';
-        }
-
-        // 抽選されたアイテム
-        // $result = Item::item_draw($rarity);
-
-        // 抽選されたレアリティで絞り込み
-        $item_box = Item::where('rarity', '=', $rarity);
-
-        // 絞り込みされたアイテムの中からランダムで取得
-        $result = $item_box->inRandomOrder()->get()->first();
-        
-        // index.blade.phpに$productsをわたす
-        return view('gacha/gacha', ['result' => $result, 'rate' => $box_rate]);
     }
 }
